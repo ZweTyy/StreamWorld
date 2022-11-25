c5773c849eb8aede4fb2f3ec0c7b81322ad0408d
zwetyasf <74769521+zwetyasf@users.noreply.github.com>
2022-11-25 11:59:31 +0100

    Paneler (virker)

--- src/GUI.java
+++ src/GUI.java
@@ -2,8 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUI {
-    // Opretter vores frame instans og alle knapper
+    // Opretter vores frame, panel og label instanser + alle knapper
     JFrame frame = new JFrame();
+    JPanel navPanel = new JPanel();
     JPanel forsidePanel = new JPanel();
     JPanel seriePanel = new JPanel();
     JPanel filmPanel = new JPanel();
@@ -16,36 +17,40 @@ public class GUI {
     protected JLabel filmLabel = new JLabel("Film!");
     protected JLabel listeLabel = new JLabel("Min Liste!");
     public GUI (){
+        // Vi kalder alle vores metoder i vores konstruktør så det er mere organiseret
         framePanels();
         frameButtons();
         frameTabs();
         frameMethods();
-
-        frame.add(forsidePanel);
-        frame.add(seriePanel);
-        frame.add(filmPanel);
-        frame.add(listePanel);
     }
     public void framePanels() {
+        /* Vi har alle vores paneler.
+           Det er her vi ændrer og definerer hvordan vores paneler skal se ud */
+        navPanel.setBackground(Color.black);
         forsidePanel.setBackground(Color.white);
         seriePanel.setBackground(Color.yellow);
         filmPanel.setBackground(Color.blue);
         listePanel.setBackground(Color.red);
 
-        forsidePanel.setBounds(0,0,1920,1080);
-        seriePanel.setBounds(0,0,1920,1080);
-        filmPanel.setBounds(0,0,1920,1080);
-        listePanel.setBounds(0,0,1920,1080);
+        navPanel.setBounds(0,0,1920,100);
+        forsidePanel.setBounds(0,100,1920,980);
+        seriePanel.setBounds(0,100,1920,980);
+        filmPanel.setBounds(0,100,1920,980);
+        listePanel.setBounds(0,100,1920,980);
+
+        navPanel.setVisible(true);
     }
     public void frameButtons() {
         // Her definerer vi knappernes dimensioner og egenskaber
-        forside.setBounds(200,25,100,20);
+        forside.setBounds(200,37,100,25);
+        serier.setBounds(325, 37, 100, 25);
+        film.setBounds(450,37,100,25);
+        minListe.setBounds(575,37,100,25);
+
+        // Focusable gør knapperne pæne (hvis focusable er true, vil der være en grim firkant rundt om teksten)
         forside.setFocusable(false);
-        serier.setBounds(325, 25, 100, 20);
         serier.setFocusable(false);
-        film.setBounds(450,25,100,20);
         film.setFocusable(false);
-        minListe.setBounds(575,25,100,20);
         minListe.setFocusable(false);
 
         // Her tilføjer vi knapperne til vores frame
@@ -55,6 +60,8 @@ public class GUI {
         frame.add(serier);
     }
     public void frameTabs() {
+        /* Der er hernede vi tilføjer funktionalitet til hver af vores knapper
+           Den fremviser den pågældende panel og sætter visibility af de andre paneler til false */
         forside.addActionListener(e -> {
             seriePanel.setVisible(false);
             filmPanel.setVisible(false);
@@ -66,6 +73,7 @@ public class GUI {
             filmPanel.setVisible(false);
             listePanel.setVisible(false);
             seriePanel.setVisible(true);
+            seriePanel.setLayout(null);
 
             serieLabel.setBounds(100,50,100,50);
             seriePanel.add(serieLabel);
@@ -76,6 +84,7 @@ public class GUI {
             seriePanel.setVisible(false);
             listePanel.setVisible(false);
             filmPanel.setVisible(true);
+            filmPanel.setLayout(null);
 
             filmLabel.setBounds(100,50,100,50);
             filmPanel.add(filmLabel);
@@ -86,6 +95,7 @@ public class GUI {
             filmPanel.setVisible(false);
             seriePanel.setVisible(false);
             listePanel.setVisible(true);
+            listePanel.setLayout(null);
 
             listeLabel.setBounds(100,50,100,50);
             listePanel.add(listeLabel);
@@ -97,5 +107,11 @@ public class GUI {
         frame.setSize(1920,1080);
         frame.setLayout(null);
         frame.setVisible(true);
+
+        frame.add(navPanel);
+        frame.add(forsidePanel);
+        frame.add(seriePanel);
+        frame.add(filmPanel);
+        frame.add(listePanel);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        GUI forside = new GUI();
+            GUI forside = new GUI();
     }
 }
