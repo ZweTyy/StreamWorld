cb2930c08a53d6bd99af3007056ff3307f1f009c
Josh0104 <69515856+Josh0104@users.noreply.github.com>
2022-11-29 12:58:53 +0100

    Ændret folder path txt i Film.java

--- src/Film.java
+++ src/Film.java
@@ -4,10 +4,10 @@ public class Film extends Medie{
 
     public static void sort(){
         Medie m = new Film();
-        m.test_sortering("./txtfiler-kopi/film.txt");
+        m.test_sortering("./txt/film.txt");
 
-        for(String i : (m.test_sortering("./txtfiler-kopi/film.txt")).keySet()){
-            System.out.println("key: " + i + " value: " + (m.test_sortering("./txtfiler-kopi/film.txt")).get(i));
+        for(String i : (m.test_sortering("./txt/film.txt")).keySet()){
+            System.out.println("key: " + i + " value: " + (m.test_sortering("./txt/film.txt")).get(i));
         }
     }
 }
